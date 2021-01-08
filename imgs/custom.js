c366816575f5da0b244d3523014abc56c5fdee08
yeji <foforh@naver.com>
2021-01-08 17:53:59 +0900

    shoppingmall

---
+++ imgs/custom.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Fetch the items form the JSON file
+function loadItems(){
+    return fetch('/user/Downloads/imgs/data/data.json').then(response => console.log(response));
+}
+
+// main
+loadItems()
+    .then (items => {
+        // displayItems(items);
+        // setEventListners(item)
+    })
+    .catch(console.log);
