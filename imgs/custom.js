486f2d41888a2cb312c59d6d1249f3e314bc3529
yeji <foforh@naver.com>
2021-01-08 19:43:27 +0900

    Update custom.js

--- imgs/custom.js
+++ imgs/custom.js
@@ -2,16 +2,19 @@
 
 // Fetch the items form the JSON file
 function loadItems(){
-    return fetch('https://yeji717.github.io/website/imgs/data.json')
+    return fetch('https://yeji717.github.io/user/Downloads/imgs/data.json')
     .then(response => response.json())
     .then(json => json.items);
 }
 
+function displayItems(items) {
+
+}
 // main
 loadItems()
     .then (items => {
         console.log(items);
-        // displayItems(items);
+        displayItems(items);
         // setEventListners(item)
     })
     .catch(console.log);
