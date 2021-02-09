f817ba4a788b0f71f3bb180d7428d9a4958bda27
yeji <foforh@naver.com>
2021-02-09 18:48:30 +0900

    0209

---
+++ new/shop/custom.js
@@ -0,0 +1,59 @@
+
+
+// Fetch the items form the JSON file
+function loadItems(){
+    return fetch('https://yeji717.github.io/website/imgs/data.json')
+    .then(response => response.json())
+    .then(json => json.items);
+}
+
+//Update the list with the given items
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+// Create HTML list item from the given data item
+function createHTMLString(item) {
+    return `
+    <li class="item">
+        <img class="item_thumbnail" src="${item.image}" alt="${item.type}">
+        <span class="item_description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
+
+function onButtonClick(event, items){
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+
+    if (key == null || value == null){
+        return;
+    }
+
+    const filtered = items.filter(item => item[key] === value);
+    displayItems(filtered);
+    // console.log(event.target.dataset.key);
+    // console.log(event.target.dataset.value);
+}
+
+function setEventListners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.button');
+    logo.addEventListener('click', ()=> displayItems(items));
+    buttons.addEventListener('click', event => onButtonClick(event, items));
+    // const imgBtn = Document.querySelector('.imgBtn');
+    // const one = imgBtn.firstChild;
+    // console.log(one);
+}
+
+// }
+// main
+loadItems()
+    .then (items => {
+        console.log(items);
+        displayItems(items);
+        setEventListners(items);
+    })
+    .catch(console.log);
